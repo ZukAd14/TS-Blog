691eefeac98b1cfb4129d85d9be2ca4a1c9fbd74
Adam Żukowski <adam.zukowski471@gmail.com>
2024-02-17 14:20:28 +0100

    Add types. Task 1.4

--- script.ts
+++ script.ts
@@ -1,41 +1,41 @@
 {
-  const selectorArticle =  '.post';
-  const selectorTitle = '.post-title';
-  const selectorTitleList = '.titles';
-  const selectorArticleTags = '.post-tags .list';
-  const selectorArticleAuthor = '.post-author';
-  const selectorAuthorsList = '.list.authors';
-  const selectorTagsList = '.sidebar .tags';
+  const selectorArticle: string =  '.post';
+const selectorTitle: string  = '.post-title';
+const selectorTitleList: string  = '.titles';
+const selectorArticleTags: string  = '.post-tags .list';
+const selectorArticleAuthor: string  = '.post-author';
+const selectorAuthorsList: string  = '.list.authors';
+const selectorTagsList: string  = '.sidebar .tags';
 
-  const titleClickHandler = function (event) {
+  const titleClickHandler = function (this: HTMLLinkElement, event: Event): void {
     event.preventDefault();
     const clickedElement = this;
 
     /* find active link and make it inactive */
-    const activeLink = document.querySelector('.titles a.active');
+    const activeLink = document.querySelector('.titles a.active') as HTMLLinkElement;
     if(activeLink) activeLink.classList.remove('active');
 
     /* add active class to clicked link */
     clickedElement.classList.add('active');
 
     /* find and hide active article */
-    const activeArticle = document.querySelector('.posts article.active');
+    const activeArticle = document.querySelector('.posts article.active') as HTMLLinkElement;
     if(activeArticle) activeArticle.classList.remove('active');
 
     /* find id of article related to clicked link, then find it and show */
-    const hrefAttribute = clickedElement.getAttribute('href');
-    const targetArticle = document.querySelector(hrefAttribute);
+    const hrefAttribute = clickedElement.getAttribute('href') as string;
+    const targetArticle = document.querySelector(hrefAttribute) as HTMLElement;
     if(targetArticle) targetArticle.classList.add('active');
   };
 
-  const generateTitleLinks = (customSelector = '') => {
+  const generateTitleLinks = (customSelector: string = ''): void => {
 
     /* find and empty title list */
-    const titleList = document.querySelector(selectorTitleList);
+    const titleList = document.querySelector(selectorTitleList) as HTMLElement;
     titleList.innerHTML = '';
 
     /* prepare variable for storing all the title links */
-    let html = '';
+    let html: string = '';
 
     /* find all articles and loop through each of them */
     const articles = document.querySelectorAll(selectorArticle + customSelector);
@@ -45,7 +45,7 @@
       const articleID = article.getAttribute('id');
 
       /* find elem that holds the title and retrieve it */
-      const articleTitle = article.querySelector(selectorTitle).innerHTML;
+      const articleTitle = (article.querySelector(selectorTitle) as HTMLElement).innerHTML;
 
       /* create HTML of the link */
       const linkHTML = '<li><a href="#' + articleID + '"><span>' + articleTitle + '</span></a></li>';
@@ -66,23 +66,23 @@
   }
 
 
-  const generateTags = () => {
+  const generateTags = (): void => {
 
     /* create a new array for holding unique tag names */
-    const allTags = [];
+    const allTags: string[] = [];
 
     /* find all articles and loop through */
     const articles = document.querySelectorAll(selectorArticle);
     for (let article of articles) {
 
       /* find div for storing tags  */
-      const tagWrapper = article.querySelector(selectorArticleTags);
+      const tagWrapper = article.querySelector(selectorArticleTags) as HTMLElement;
 
           /* prepare variable for storing all the tag links */
-      let html = '';
+      let html: string = '';
 
       /* get info about tags from data-tags attribute */
-      const dataTag = article.getAttribute('data-tags');
+      const dataTag = article.getAttribute('data-tags') as string;
 
       /* split tags into array */
       const tagsArray = dataTag.split(' ');
@@ -107,10 +107,10 @@
     }
 
     /* find tags list in sidebar */
-    const tagList = document.querySelector(selectorTagsList);
+    const tagList = document.querySelector(selectorTagsList) as HTMLElement;
 
     /* create variable for all links */
-    let allTagsHTML = '';
+    let allTagsHTML: string = '';
 
     /* loop for each tag in unique tags list */
     for (let tag of allTags) {
@@ -121,20 +121,20 @@
     tagList.innerHTML = allTagsHTML;
   }
 
-  const generateAuthors = () => {
+  const generateAuthors = (): void => {
 
     /* create list of unique authors */
-    let allAuthors = [];
+    let allAuthors: string[] = [];
 
     /* find all articles and loop through */
     const articles = document.querySelectorAll(selectorArticle);
     for (let article of articles) {
 
       /* find wrapper for author in article elem */
-      const articleAuthor = article.querySelector(selectorArticleAuthor);
+      const articleAuthor = article.querySelector(selectorArticleAuthor) as HTMLElement;
 
       /* get article data-author attribute */
-      const author = article.getAttribute('data-author');
+      const author = article.getAttribute('data-author') as string;
 
       /* check if author is not already in the list, if not -> push it */
       if (!allAuthors.includes(author)) {
@@ -147,7 +147,7 @@
     }
 
     /* find wrapper for author links in sidebar */
-    const authorList = document.querySelector(selectorAuthorsList)
+    const authorList = document.querySelector(selectorAuthorsList) as HTMLElement
 
     /* loop through unique authors and generate author links in in sidebar*/
     for (let author of allAuthors) {
